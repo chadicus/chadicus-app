e9bafb8e4630e094f471678a090ef9188bbc0967
chadicus <chadwickgray@gmail.com>
2014-02-10 22:35:56 -0500

    Use marvel client

--- src/routes.php
+++ src/routes.php
@@ -3,4 +3,17 @@ return function(\Slim\Slim $app) {
     $app->get('/', function() use($app) {
         $app->render('home.html', ['page' => 'home']);
     })->name('home');
+
+    $app->get('/hulk', function() use ($app) {
+
+        $client = new \Chadicus\Marvel\Api\Client(
+            getenv('MARVEL_PRIVATE_KEY'),
+            getenv('MARVEL_PUBLIC_KEY'),
+            new \Chadicus\Marvel\Api\CurlAdapter()
+        );
+
+        $response = $client->search('comics', ['characters' => 1009351]);
+
+        $app->render('hulk.html', ['comics' => $response->getBody()['data']['results']]);
+    })->name('hulk');
 };
