8aebb593af733f449f522c8d1b9c706ea0405d8e
chadicus <chadwickgray@gmail.com>
2014-03-19 23:06:00 -0400

    Add inventory route

--- src/routes.php
+++ src/routes.php
@@ -16,4 +16,25 @@ return function(\Slim\Slim $app) {
 
         $app->render('hulk.html', ['comics' => $response->getBody()['data']['results']]);
     })->name('hulk');
+
+    $app->get('/inventory', function() use($app) {
+        $limit = isset($_GET['limit']) ? (int)$_GET['limit'] : 10;
+        $offset = isset($_GET['offset']) ? (int)$_GET['offset'] : 0;
+        $inventory = [
+            [
+                'name' => '',
+            ],
+        ];
+
+        $items = array_slice($inventory, $offset, $limit);
+        $result = [
+            'total' => count($inventory),
+            'limit' => count($items),
+            'offset' => $offset,
+            'items' => $inventory,
+        ];
+
+        header('Content-Type: application/json');
+        echo json_encode($result);
+    })->name('inventory');
 };
